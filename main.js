beb5b9a4b0c76470625ebfade9e5eaa1b1ae7c38
daneparke <dapa8522@colorado.edu>
2018-10-15 09:10:37 -0600

    Balloon Fun

---
+++ main.js
@@ -0,0 +1,16 @@
+// var audio = document.getElementById("mySoundClip");
+// var balloon = document.querySelector('.grow')
+
+// balloon.addEventListener('mouseenter', playAudio)
+// function playAudio() {
+//     audio.play();
+// }
+// var audio = $("#mySoundClip")[0];
+// $(".grow").mouseenter(function () {
+//     audio.play();
+// });
+
+// function play() {
+//     var audio = document.getElementById("audio");
+//     audio.play();
+// }
